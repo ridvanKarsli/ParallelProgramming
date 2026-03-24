6892cc6ce2b2f11435a77a2e719218a741a7513e
Ebrukoksal <114864294+Ebrukoksal@users.noreply.github.com>
2026-03-24 21:33:51 +0300

    Add performance decorator with memory and time tracking
    Implement a performance decorator to measure execution time and memory usage.

---
+++ Week04/decorators_ebru_koksal.py
@@ -0,0 +1,24 @@
+import time
+import tracemalloc
+
+def performance(func):
+    def wrapper(*args, **kwargs):
+        tracemalloc.start()
+        t1 = time.perf_counter()
+        
+        result = func(*args, **kwargs)
+        
+        t2 = time.perf_counter()
+        _, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        
+        performance.counter += 1
+        performance.total_time += (t2 - t1)
+        performance.total_mem += peak
+        
+        return result
+    return wrapper
+
+performance.counter = 0
+performance.total_time = 0
+performance.total_mem = 0
